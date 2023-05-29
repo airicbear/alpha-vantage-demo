c8ed35fc20e8aae5bf5e0c1ff604a962281a37f0
Eric Nguyen <etndevel@gmail.com>
2023-05-29 15:57:54 -0400

    Refactor url search params

--- app/api/intraday/route.ts
+++ app/api/intraday/route.ts
@@ -1,13 +1,18 @@
 import { NextResponse } from "next/server";
 
 export async function GET(req: Request) {
-  const url =
-    "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=demo";
+  const url = "https://www.alphavantage.co/query?";
 
-  const res = await fetch(url, {
+  const urlParams = new URLSearchParams({
+    function: "TIME_SERIES_INTRADAY",
+    symbol: "IBM",
+    interval: "5min",
+    apikey: "demo",
+  });
+
+  const res = await fetch(url + urlParams, {
     headers: {
       "Content-Type": "application/json",
-      "API-Key": process.env.API_KEY,
     },
   });
   const data = await res.json();
