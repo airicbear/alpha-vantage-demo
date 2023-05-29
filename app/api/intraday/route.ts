73247cd69fd28cfe41605d7ee49ab624dae34a45
Eric Nguyen <etndevel@gmail.com>
2023-05-29 15:15:41 -0400

    Add intraday API

---
+++ app/api/intraday/route.ts
@@ -0,0 +1,18 @@
+import { NextResponse } from "next/server";
+
+var request = require("request");
+
+export async function GET(req: Request) {
+  const url =
+    "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=demo";
+
+  const res = await fetch(url, {
+    headers: {
+      "Content-Type": "application/json",
+      "API-Key": process.env.API_KEY,
+    },
+  });
+  const data = await res.json();
+
+  return NextResponse.json({ data });
+}
